dda817f3d10977d9090a8d4976ef6cc31d1cdab4
Pohrebniak Andrian <pogra29@gmail.com>
2024-03-12 13:23:57 +0200

    Done 4

--- src/4.ts
+++ src/4.ts
@@ -1,19 +1,39 @@
-
 class Key {
-    constructor() {
-        console.log('Key is created');
-    }
-
-    public open() {
-        console.log('Door is open');
-    }
+  private signature: number = Math.random();
+  public getSignature(): number {
+    return this.signature;
+  }
 }
-    
 
+class Person {
+  constructor(private key: Key) {}
+  public getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenant: Person[] = [];
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenant.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
 
-
-
-
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -22,6 +42,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
